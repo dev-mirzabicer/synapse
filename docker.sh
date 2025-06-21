a4e3233f50e833105bd04e186a623b84dd431d30
MirzaBicer <mirzabicer.dev@gmail.com>
2025-06-21 22:35:57 +0000

    force recreate option in docker compose up

--- docker.sh
+++ docker.sh
@@ -5,5 +5,7 @@ set -e
 rm -rf ./backend/api_gateway/alembic/versions/*
 
 docker-compose down -v
-docker-compose up -d --build
+docker-compose up -d --build --force-recreate
+# Wait for the database to be ready
+sleep 10
 docker-compose run --rm api_gateway bash -c "alembic revision --autogenerate -m 'Initial schema with chat groups' && alembic upgrade head"
