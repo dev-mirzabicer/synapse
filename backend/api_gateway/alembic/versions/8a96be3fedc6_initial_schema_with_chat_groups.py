2ef0b0f3cdb3eb2a63500f9cb807ddf0c9b775c7
MirzaBicer <mirzabicer.dev@gmail.com>
2025-06-22 14:34:02 +0000

    feat: add initial schema with chat groups and messages tables

--- backend/api_gateway/alembic/versions/8a96be3fedc6_initial_schema_with_chat_groups.py
+++ backend/api_gateway/alembic/versions/8a96be3fedc6_initial_schema_with_chat_groups.py
@@ -1,8 +1,8 @@
 """Initial schema with chat groups
 
-Revision ID: cb6856a0f0ef
+Revision ID: 8a96be3fedc6
 Revises: 
-Create Date: 2025-06-22 13:13:19.227193
+Create Date: 2025-06-22 14:25:38.610793
 
 """
 from typing import Sequence, Union
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = 'cb6856a0f0ef'
+revision: str = '8a96be3fedc6'
 down_revision: Union[str, Sequence[str], None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
