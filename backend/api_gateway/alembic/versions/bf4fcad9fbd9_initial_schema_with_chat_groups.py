cc822e84e1ee074fa686be4600c6c15a831fb6ac
MirzaBicer <mirzabicer.dev@gmail.com>
2025-06-23 15:04:00 +0000

    alembic migrations

--- backend/api_gateway/alembic/versions/bf4fcad9fbd9_initial_schema_with_chat_groups.py
+++ backend/api_gateway/alembic/versions/bf4fcad9fbd9_initial_schema_with_chat_groups.py
@@ -1,8 +1,8 @@
 """Initial schema with chat groups
 
-Revision ID: 9d11a91839f7
+Revision ID: bf4fcad9fbd9
 Revises: 
-Create Date: 2025-06-23 14:33:24.192684
+Create Date: 2025-06-23 14:49:36.127432
 
 """
 from typing import Sequence, Union
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = '9d11a91839f7'
+revision: str = 'bf4fcad9fbd9'
 down_revision: Union[str, Sequence[str], None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
